5af059cef51b4d69e2eb004f7f8f59098b1f348e
jhaago <jordanhaagensen@gmail.com>
2026-09-10 01:38:56 +0800

    Package CrowdLight Bridge v0.5

--- CrowdLightBridge/scripts/build-app.sh
+++ CrowdLightBridge/scripts/build-app.sh
@@ -44,9 +44,9 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>0.4.0</string>
+    <string>0.5.0</string>
     <key>CFBundleVersion</key>
-    <string>4</string>
+    <string>5</string>
     <key>LSMinimumSystemVersion</key>
     <string>11.0</string>
     <key>LSApplicationCategoryType</key>
